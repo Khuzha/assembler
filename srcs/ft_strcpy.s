f0fb3bddf3550d9b1a0fffaa058717c0de58950b
khuzha <mr.khuzha@gmail.com>
2021-05-03 20:33:53 +0300

    add ft_strcpy

---
+++ srcs/ft_strcpy.s
@@ -0,0 +1,13 @@
+global _ft_strcpy					; make _ft_strlen avialible outer from module
+
+section		.text					; define code memory section
+_ft_strcpy:	xor rax, rax			; put zero to rax
+.loop:		mov	bl, byte[rsi + rax]	;
+			mov byte[rdi + rax], bl	;
+			cmp bl, 0				;
+			je	.return				;
+			inc	rax					;
+			jmp .loop				;
+
+.return:	mov rax, rdi			;
+			ret						; exit and return rax
